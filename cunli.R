18fdd552538a5c7dec1590b4eb23c0561403b215
rujinlong <jinlongru@gmail.com>
2026-01-29 15:17:12 +0100

    clean code

--- cunli.R
+++
@@ -1,5 +0,0 @@
-get_viruses_by_group <- function(tpm_mat, sample_groups, group_name) {
-  samples <- names(sample_groups)[sample_groups == group_name]
-  group_tpm <- tpm_mat[, samples, drop = FALSE]
-  rownames(group_tpm)[rowSums(group_tpm) > 0]
-}
